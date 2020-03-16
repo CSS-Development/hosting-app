7d122d74a3e2edaebe5fc3c26a63ffa5762bd773
Brayan Garcia <bgarcia.95@outlook.com>
2020-03-16 15:11:35 -0600

    adding events to our sidebar and modal

--- shared.js
+++ shared.js
@@ -1,5 +1,44 @@
 var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButton = document.querySelectorAll(".plan button");
+var modalCancelButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+console.dir(backdrop);
 
-// console.dir(backdrop);
+// backdrop.style.display = "block";\
+// console.dir(selectPlanButton);
+for (var i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener("click", function() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = "open"; // This will actually override the complete class list
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
+}
 
-backdrop.style.display = "block";
+if (modalCancelButton) {
+  modalCancelButton.addEventListener("click", closeModal);
+}
+
+backdrop.addEventListener("click", function() {
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+function closeModal() {
+  //   modal.style.display = "none";
+  //   backdrop.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function() {
+  //   backdrop.style.display = "block";
+  //   mobileNav.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
